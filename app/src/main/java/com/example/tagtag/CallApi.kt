24d38054e3e628999031940db5d708a31f4dbb56
oneRicoder <rinkusingh805764>
2022-04-26 21:43:41 +0530

    changed call api url from constant

--- app/src/main/java/com/example/tagtag/CallApi.kt
+++ app/src/main/java/com/example/tagtag/CallApi.kt
@@ -5,6 +5,6 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface CallApi {
-    @GET(Constants.url)
+    @GET("youtube/v3/videos?part=snippet&id=2k7aHIDQc1s&key=AIzaSyDcMzWLqlRMYqH3LaYih3ykwp25zcvhVA0")
     fun callVideoApi(): Call<VideoDetails>
 }
