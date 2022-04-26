e55a0a48ba093974c5ef8235638a238a3503a001
oneRicoder <rinkusingh805764>
2022-04-27 00:36:50 +0530

    Completed Api related work

--- app/src/main/java/com/example/tagtag/CallApi.kt
+++ app/src/main/java/com/example/tagtag/CallApi.kt
@@ -3,8 +3,14 @@ package com.example.tagtag
 import com.example.tagtag.json.VideoDetails
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface CallApi {
-    @GET("youtube/v3/videos?part=snippet&id=2k7aHIDQc1s&key=AIzaSyDcMzWLqlRMYqH3LaYih3ykwp25zcvhVA0")
-    fun callVideoApi(): Call<VideoDetails>
+    @GET("youtube/v3/videos")
+    fun callVideoApi(
+        @Query("part") part: String,
+        @Query("id") id: String,
+        @Query("key") apiKey: String
+    ): Call<VideoDetails>
 }
--- app/src/main/java/com/example/tagtag/MainActivity.kt
+++ app/src/main/java/com/example/tagtag/MainActivity.kt
@@ -1,46 +1,81 @@
 package com.example.tagtag
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.tagtag.json.VideoDetails
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
+    private var VIDEO_ID: String = ""
+    private var URl = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        getJsonDataUsingRetrofit()
+
+
+        search_btn.setOnClickListener {
+            URl = editText.text.toString()
+            VIDEO_ID = getYouTubeId(URl)
+            Log.i("foundndnd",VIDEO_ID)
+            if (VIDEO_ID.isEmpty() || VIDEO_ID.equals("error")){
+                Toast.makeText(this, "Video Id Not Found!", Toast.LENGTH_SHORT).show()
+            }else{
+                getJsonDataUsingRetrofit(VIDEO_ID)
+            }
+
+
+        }
+
+
     }
 
-    private fun getJsonDataUsingRetrofit(){
+    private fun getYouTubeId(youTubeUrl: String): String {
+        val pattern = "(?<=youtu.be/|watch\\?v=|/videos/|embed\\/)[^#\\&\\?]*"
+        val compiledPattern = Pattern.compile(pattern)
+        val matcher = compiledPattern.matcher(youTubeUrl)
+        return if (matcher.find()) {
+            matcher.group()
+        } else {
+            "error"
+        }
+    }
+
+    private fun getJsonDataUsingRetrofit(videoId: String){
         val retrofit = Retrofit.Builder().baseUrl("https://youtube.googleapis.com/")
             .addConverterFactory(GsonConverterFactory.create()).build()
 
-        val service: CallApi = retrofit.create<CallApi>(CallApi::class.java)
-        val listCall: Call<VideoDetails> = service.callVideoApi()
+        val service: CallApi = retrofit.create(CallApi::class.java)
+        val listCall: Call<VideoDetails> = service.callVideoApi("snippet", videoId,"AIzaSyDcMzWLqlRMYqH3LaYih3ykwp25zcvhVA0")
         listCall.enqueue(object : Callback<VideoDetails> {
             override fun onResponse(call: Call<VideoDetails>, response: Response<VideoDetails>) {
                 if (response.isSuccessful){
                     val check : VideoDetails = response.body() as VideoDetails
-                    Log.i("success_success",check.items[0].snippet.tags.toString())
+                    showTagsInRecyclerView(check.items[0].snippet.tags as ArrayList<String>)
                 }
             }
 
             override fun onFailure(call: Call<VideoDetails>, t: Throwable) {
-                TODO("Not yet implemented")
+                Toast.makeText(this@MainActivity,"Sorry Something Went Wrong!", Toast.LENGTH_SHORT).show()
             }
-
         })
     }
 
-    fun videoId() : String {
-        val result: String = "2k7aHIDQc1s"
-        return result
+    private fun showTagsInRecyclerView(tagList: ArrayList<String>){
+        if (tagList.isNullOrEmpty()){
+            Log.i("success_success","No Tags Available")
+        }else{
+            for (i in tagList){
+                Log.i("success_success",i)
+            }
+        }
     }
 }
